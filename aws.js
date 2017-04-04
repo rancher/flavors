11fc14238831aa9dd415f1323f5af2660a4eda58
Westly Wright <westly.wright@gmail.com>
2017-04-04 11:48:15 -0700

    DO and AWS Working

--- aws.js
+++ aws.js
@@ -1,45 +1,59 @@
+/* eslint-env node */
+const YAML = require('yamljs');
 const fs = require('fs');
-const safeInstances = ["t2.micro", "t2.small", "t2.medium", "t2.large", 't2.xlarge', "t2.2xlarge", "m3.medium", "m4.large", "m4.xlarge", "m4.2xlarge", "m4.4xlarge"];
+const SAFEINSTANCES = ["t2.micro", "t2.small", "t2.medium", "t2.large", 't2.xlarge', "t2.2xlarge", "m3.medium", "m4.large", "m4.xlarge", "m4.2xlarge", "m4.4xlarge"];
+
 function parseAWS() {
-    var filelist = [];
-    var contents = fs.readFileSync("aws_instances.json");
-    // Define to JSON type
-    var jsonContent = JSON.parse(contents);
-    // we only want linux
-    // monthly cost is hourly*720=mo
-    // storage is 40gb default
-    function isInstanceSafe(instance) {
-        var instanceType = instance.instance_type.split('.')[0].toLowerCase(); 
-        switch (instanceType) {
-            case 't2':
-            case 'm3':
+  var filelist = [];
+  var contents = fs.readFileSync("aws_instances.json");
+  var jsonContent = JSON.parse(contents);
+  var filelist = [];
+  jsonContent.forEach((awsi) => {
+    if (SAFEINSTANCES.indexOf(awsi.instance_type) !== -1) {
+      Object.keys(awsi.pricing).forEach((zone) => {
+        // do we have ondmand pricing?
+        if (awsi.pricing[zone].linux.ondemand) {
+          var id = `amazonec2-${zone}-${awsi.instance_type.split('.').join('-')}`;
+          filelist.push({
+            provider: "amazonec2",
+            driver_options: {
+              "amazonec2-instance-type": awsi.instance_type,
+              "amazonec2-region": zone,
+              "amazonec2-zone": "a",
+            },
+            ui_options: {
+              id: id,
+              display_name: `${awsi.pretty_name} (${zone})`,
+              memory: awsi.memory,
+              storage: 40,
+              zone: zone,
+              price_per_hour: awsi.pricing[zone].linux.ondemand,
+              price_per_month: (awsi.pricing[zone].linux.ondemand) * 720,
+              variable_fees: true,
+              cpu_rating: Math.floor(Math.random() * 5) + 1,
+              disk_rating: Math.floor(Math.random() * 5) + 1,
+              transfer: awsi.ebs_max_bandwidth,
+              realm: null,
+              api_key_support: 'byokey',
+            },
+          });
         }
+      });
+      filelist.forEach((fileobj, idx) => {
+        fs.writeFile(`flavors/${fileobj.ui_options.id}.yaml`, YAML.stringify(fileobj, 4), function (err) {
+          if (err) {
+            console.log(err);
+            process.exit(1);
+          } else {
+            console.log("The file was saved!");
+              if (idx === filelist.length -1) {
+                console.log('Finished writing files!');
+                process.exit(0);
+              }
+          }
+        });
+      });
     }
-    jsonContent.forEach((awsi) => {
-        console.log(awsi.generation);
-        var instanceType =awsi.instance_type.split('.')[0].toLowerCase(); 
-        if ( === 't2')
-        // filelist.push({
-        //     provider: "amazon",
-        //     driver_options: {
-        //     },
-        //     ui_options: {
-        //         id: `${}-${}-${}`,
-        //         display_name: '',
-        //         memory: null,
-        //         storage: null,
-        //         zone: null,
-        //         price_per_hour: null,
-        //         price_per_month: null,
-        //         variable_fees: true,
-        //         cpu_rating: Math.floor(Math.random() * 5) + 1,
-        //         disk_rating: Math.floor(Math.random() * 5) + 1,
-        //         transfer: null,
-        //         realm: null,
-        //         api_key_support: 'byokey',
-        //     },
-        // });
-        debugger;
-    });
+  });
 }
 parseAWS();
--- do.js
+++ do.js
@@ -1,55 +1,108 @@
 /* eslint-env node */
+
+const PDN = {
+  "1gb": {
+    name: 'Mini'
+  },
+  "2gb": {
+    name: 'Small'
+  },
+  "4gb": {
+    name: 'Medium'
+  },
+  "8gb": {
+    name: 'Large'
+  },
+  "16gb": {
+    name: 'Extra Large'
+  },
+  "32gb": {
+    name: 'Mega'
+  },
+  "48gb": {
+    name: 'Gigantic'
+  },
+  "64gb": {
+    name: 'Colossal'
+  },
+};
+
+const REALMS = {
+  "nyc1": "US East",
+  "ams1": "EU East",
+  "sfo1": "US West",
+  "sfo2": "US West",
+  "nyc2": "US East",
+  "ams2": "EU East",
+  "sgp1": "Asia",
+  "lon1": "EU West",
+  "nyc3": "US East",
+  "ams3": "EU East",
+  "fra1": "EU West",
+  "tor1": "CAN East",
+  "blr1": "Asia",
+}
+
 const YAML = require('yamljs');
 const digitalocean = require('digitalocean');
-const client = digitalocean.client('6e55bcbb3179cb7e312b5074ca9f933445e72c8dc7d640142d2370413fe9906b');
+const client = digitalocean.client('');
 const fs = require('fs');
 
 function getRegions() {
-    var regionList = {};
-    var filelist = [];
-    // the display name can be a static map of file names = 1gb = DO SFO1 Small
-    // drop any m-* hosts we dont need thses yet
-    client.sizes.list().then((sizes) => {
-        debugger;
-        sizes.forEach((size) => {
-            size.regions.forEach((reg) => {
-                filelist.push({
-                    provider: "digitalocean",
-                    driver_options: {
-                        "digitalocean-image": "ubuntu-16-04-x64",
-                        "digitalocean-ipv6": false,
-                        "digitalocean-private-networking": false,
-                        "digitalocean-backups": false,                       
-                    },
-                    ui_options: {
-                        id: `digitalocean-${reg}-${size.slug}`,
-                        display_name: '',
-                        memory: size.memory,
-                        storage: size.disk,
-                        zone: reg,
-                        price_per_hour: size.price_hourly,
-                        price_per_month: size.price_monthly,
-                        variable_fees: false,
-                        cpu_rating: Math.floor(Math.random() * 5) + 1,
-                        disk_rating: Math.floor(Math.random() * 5) + 1,
-                        transfer: size.transfer,
-                        realm: null,
-                        api_key_support: 'managed',
-                    },
-                });
-            });
+  // just in case we want to get a full list of regions available to us
+  // client.regions.list().then((reg) => {
+  //   reg.forEach((r) => {
+  //     console.log(r.slug);
+  //   });
+  // });
+  client.sizes.list().then((sizes) => {
+    let filelist = [];
+    sizes.forEach((size) => {
+      size.regions.forEach((reg) => {
+        if (size.slug !== '512mb' && !size.slug.startsWith('m-')) {
+          var id = `digitalocean-${reg}-${size.slug}`;
+          filelist.push({
+            provider: "digitalocean",
+            driver_options: {
+              "digitalocean-image": "ubuntu-16-04-x64",
+              "digitalocean-ipv6": false,
+              "digitalocean-private-networking": false,
+              "digitalocean-backups": false,
+            },
+            ui_options: {
+              id: id,
+              display_name: `Digital Ocean ${PDN[size.slug].name} (${reg})`,
+              memory: size.memory,
+              storage: size.disk,
+              zone: reg,
+              price_per_hour: size.price_hourly,
+              price_per_month: size.price_monthly,
+              variable_fees: false,
+              cpu_rating: Math.floor(Math.random() * 5) + 1,
+              disk_rating: Math.floor(Math.random() * 5) + 1,
+              transfer: size.transfer,
+              realm: REALMS[reg],
+              api_key_support: 'managed',
+            },
+          });
+        }
+      });
+      filelist.forEach((fileobj, idx) => {
+        fs.writeFile(`flavors/${fileobj.ui_options.id}.yaml`, YAML.stringify(fileobj, 4), function (err) {
+          if (err) {
+            console.log(err);
+            process.exit(1);
+          } else {
+            console.log("The file was saved!");
+            if (idx === filelist.length - 1) {
+              console.log('Finished writing files!');
+              process.exit(0);
+            }
+          }
         });
-        filelist.forEach((fileobj) => {
-            fs.writeFile(`flavors/${fileobj.ui_options.id}.yaml`, YAML.stringify(fileobj, 16), function(err) {
-                if(err) {
-                    console.log(err);
-                } else {
-                    // console.log("The file was saved!");
-                }
-            }); 
-        });
-        // process.exit(1);
+      });
     });
+  });
 }
 
 getRegions();
